08e5109081b8473346b49b918a3eb7087cec867f
dm4rt <danmartinez2004@gmail.com>
2024-03-17 00:04:48 -0400

    created a simple addRow function that tracks rows and cols

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 //Global Vars
-let rows = 0;
-let cols = 0;
+let gridRows = 0;
+let gridCols = 0;
+
+//insert cell
+function insertCell(input){
+	let td = document.createElement("td");
+	
+	input.appendChild(td);
+}
 
 //add row
 function addRow() {
-	
+	let row = document.createElement("tr");
+	//Special case where no cols yet
+	if(gridCols<=0){
+		insertCell(row);
+		gridCols++;
+	}
+	else{
+		for(let x=0;x<gridCols;x++){
+			insertCell(row);
+		}
+	}
+	document.getElementById("grid").appendChild(row);
+	gridRows++;
+	console.log(gridRows, gridCols);	
 }
